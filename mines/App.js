dddf09d5ac6cee7e94faec5bd4f9f04ede26fd12
leoderigo <leoderigo.sjb@gmail.com>
2021-11-28 11:39:45 -0300

    Correção na finalização do jogo

--- mines/App.js
+++ mines/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView, StyleSheet, Text, View, Alert, Vibration } from 'react-native'
 import Header from './src/components/Header'
 import LevelSelection from './src/components/LevelSelection'
@@ -25,22 +25,26 @@ export default () => {
   let clonedBoard = cloneBoard(board)
   let lost = false
   let won = false
-  const [gameStop, setGameStop] = useState(false)
+  const [gameState, setGameState] = useState('normal')
   const [levelSelectionVisible, setLevelSelectionVisible] = useState(false)
 
+  useEffect(() => {
+    if (gameState !== 'normal')
+      Alert.alert((gameState === 'lost') ? 'KKKKKKKK burrão' : 'Aee po, te amo lindeza <3')
+  }, [gameState])
+
   const onOpenField = (row, column) => {
-    if (gameStop) return
     openField(clonedBoard, row, column)
     lost = hadExplosion(clonedBoard)
     won = wonGame(clonedBoard)
 
     if (lost) {
       showMines(clonedBoard)
-      setGameStop(true)
+      setGameState('lost')
       Alert.alert('KKKKKKKK burrão')
     }
     if (won) {
-      setGameStop(true)
+      setGameState('won')
       Alert.alert('Aee po, te amo lindeza <3')
     }
     setBoard(clonedBoard)
@@ -55,7 +59,7 @@ export default () => {
   const onNewGame = () => {
     clonedBoard = createMinedBoard(rows, columns, minesAmount(params.difficult))
     setBoard(clonedBoard)
-    setGameStop(false)
+    setGameState('normal')
   }
 
   const onLevelSelection = difficult => {
